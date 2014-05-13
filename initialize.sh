3105031f5e1d9952d481e505523aead7d8a54337
Joshua Wascom <jkwascom@gmail.com>
2014-05-13 19:26:01 +0000

    updating proto-initialize script to be user agnostic

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,14 @@
 #need to add some logic to find root of cloud env based on script location
 TARGET_DIR=$HOME
 CLOUD_ENV_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CLOUD_SCRIPT_ROOT=$CLOUD_ENV_ROOT/arch
-LOCAL_ZSH_SCRIPT="$HOME/.zshrc"
+LOCAL_ZSH_SCRIPT="$TARGET_DIR/.zshrc"
 
 [[ -e $LOCAL_ZSH_SCRIPT ]] && echo "$LOCAL_ZSH_SCRIPT already exists!" && exit -1
 
-echo 'source "$HOME/general-environment-setup/configs/.zshrc"' >> $LOCAL_ZSH_SCRIPT
+echo "export CLOUD_ENV_ROOT=\"$CLOUD_ENV_ROOT\"" >> $LOCAL_ZSH_SCRIPT
+echo "export CLOUD_SCRIPT_ROOT=\"$CLOUD_ENV_ROOT/arch\"" >> $LOCAL_ZSH_SCRIPT
+echo "export CLOUD_CONFIG_ROOT=\"$CLOUD_ENV_ROOT/configs\"" >> $LOCAL_ZSH_SCRIPT
+echo 'source "$CLOUD_CONFIG_ROOT/$USER.zshrc"' >> $LOCAL_ZSH_SCRIPT
 
 #export HOSTINFO=`ip link show`
 #export HOSTINFO_DIGEST=`echo $HOSTINFO | md5sum`
