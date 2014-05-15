7874cd74f8b0b9787d48fa194f4282f087517078
Nicholas Scheurich <nick@scheurich.me>
2014-05-15 13:24:03 -0500

    Source .vimrc for current user

--- initialize.sh
+++ initialize.sh
@@ -2,6 +2,7 @@
 TARGET_DIR=$HOME
 CLOUD_ENV_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOCAL_ZSH_SCRIPT="$TARGET_DIR/.zshrc"
+LOCAL_VIM_SCRIPT="$TARGET_DIR/.vimrc"
 
 [[ -e $LOCAL_ZSH_SCRIPT ]] && echo "$LOCAL_ZSH_SCRIPT already exists!" && exit -1
 
@@ -9,6 +10,10 @@ echo "export CLOUD_ENV_ROOT=\"$CLOUD_ENV_ROOT\"" >> $LOCAL_ZSH_SCRIPT
 echo "export CLOUD_SCRIPT_ROOT=\"$CLOUD_ENV_ROOT/arch\"" >> $LOCAL_ZSH_SCRIPT
 echo "export CLOUD_CONFIG_ROOT=\"$CLOUD_ENV_ROOT/configs\"" >> $LOCAL_ZSH_SCRIPT
 echo 'source "$CLOUD_CONFIG_ROOT/$USER.zshrc"' >> $LOCAL_ZSH_SCRIPT
+
+[[ -e $LOCAL_VIM_SCRIPT ]] && echo "$LOCAL_VIM_SCRIPT already exists!" && exit -1
+
+echo "source $CLOUD_ENV_ROOT/configs/$USER.vimrc" >> $LOCAL_VIM_SCRIPT
 
 #export HOSTINFO=`ip link show`
 #export HOSTINFO_DIGEST=`echo $HOSTINFO | md5sum`
